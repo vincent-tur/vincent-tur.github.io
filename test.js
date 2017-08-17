e51e5ae38fee186e241b7096dce4f3c6cb5bb677
vincent-tur <vincentturato@gmail.com>
2017-08-17 14:49:53 -0700

    s

--- test.js
+++ test.js
@@ -1,8 +1,5 @@
 var url = "https://toggl.com/reports/api/v2/details?user_agent=fa_vince@hotmail.com&workspace_id=2204314&since=2017-08-01&until=2017-08-17&page=";
 
-
-for(q = 1; q <= len; q++){
-	async_request.push(
 	$.ajax({
 		type: 'GET',
 		url: url + q,
@@ -16,6 +13,4 @@ for(q = 1; q <= len; q++){
 				console.log(tasks[i]);
 			}
 		}
-	})
-	);
-}
+	});
