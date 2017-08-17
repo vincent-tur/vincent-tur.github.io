9509fbcf8a4fd89e54123aa35b41e5d909cb2c4d
vincent-tur <vincentturato@gmail.com>
2017-08-17 14:47:54 -0700

    4

--- test.js
+++ test.js
@@ -13,25 +13,7 @@ for(q = 1; q <= len; q++){
 		success: function (resp) {
 			var tasks = resp.data; 
 			for (var i = 0, len = tasks.length; i < len; i++){
-				desc = tasks[i].description;
-				start = tasks[i].start.substring(0,10) + " " + tasks[i].start.substring(11,18);
-				end = tasks[i].end.substring(0,10) + " " + tasks[i].end.substring(11,18);
-				proj = tasks[i].project;
-				task = tasks[i].task;
-				client = tasks[i].client;
-				bill = tasks[i].billable;
-				isbill = tasks[i].is_billable;
-				
-				tableData.push({
-					"desc" : desc,
-					"start" : start,
-					"end" : end,
-					"proj" : proj,
-					"tasl" : task,
-					"client" : client,
-					"bill" : bill,
-					"isbill" : isbill
-				});
+				console.log(tasks[i]);
 			}
 		}
 	})
