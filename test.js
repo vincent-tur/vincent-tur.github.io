568f1219c1f05aa47f67bf4f4b52faffd71a7280
vincent-tur <vincentturato@gmail.com>
2017-08-17 14:50:24 -0700

    vv

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-var url = "https://toggl.com/reports/api/v2/details?user_agent=fa_vince@hotmail.com&workspace_id=2204314&since=2017-08-01&until=2017-08-17&page=";
+var url = "https://toggl.com/reports/api/v2/details?user_agent=fa_vince@hotmail.com&workspace_id=2204314&since=2017-08-01&until=2017-08-17&page=1";
 
 	$.ajax({
 		type: 'GET',
-		url: url + q,
+		url: url,
 		dataType: 'json',
 		beforeSend: function (xhr) {
 			xhr.setRequestHeader('Authorization', 'Basic ' + btoa('d323f7bda7be76cc85b7836db3eaafab:api_token'));
