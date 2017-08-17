7f3803dd081b9a3089051d84aef581435a0469ea
vincent-tur <vincentturato@gmail.com>
2017-08-17 15:08:30 -0700

    f

--- togglwdc.js
+++ togglwdc.js
@@ -105,16 +105,17 @@ function pagesCallback(data){
 							"client" : client,
 						};
 
-						var tags = tasks[i].tags;
+						// var tags = tasks[i].tags;
+						// if(tags.length > 0){
+						// 	for(b = 0; b <= tags.length; b++){
+						// 		rowObj.tags = tags[b];
+						// 		tableData.push(rowObj);
+						// 	}
+						// }else{
+						// 	tableData.push(rowObj);
+						// }
 
-						if(tags.length > 0){
-							for(b = 0; b <= tags.length; b++){
-								rowObj.tags = tags[b];
-								tableData.push(rowObj);
-							}
-						}else{
-							tableData.push(rowObj);
-						}
+						tableData.push(rowObj);
 
 						
 					}
